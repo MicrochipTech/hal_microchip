0980c70953245583c5802e415b8a6c45cbee738e
Scott Worley <scott.worley@microchip.com>
2025-07-10 18:38:16 -0400

    hal: microchip: mec5: Convert mailbox to MMCR register style.
    We converted the mailbox peripheral code to MMCR register access.
    
    Signed-off-by: Scott Worley <scott.worley@microchip.com>

--- mec5/drivers/mec_mailbox.c
+++ mec5/drivers/mec_mailbox.c
@@ -7,20 +7,13 @@
 #include <stdint.h>
 
 #include <device_mec5.h>
-#include "mec_pcfg.h"
+#include <mec_mailbox_regs.h>
 #include "mec_defs.h"
 #include "mec_ecia_api.h"
 #include "mec_pcr_api.h"
 #include "mec_mailbox_api.h"
 #include "mec_retval.h"
-
-#define MEC_MBOX_GIRQ 15
-#define MEC_MBOX_GIRQ_POS 20
-
-#define MEC_MBOX0_ECIA_INFO MEC5_ECIA_INFO(15, 20, 7, 60)
-
-/* Each mailbox device implements 32 8-bit mailboxes */
-#define MEC_MBOX_MAX_INDEX 31
+#include "mec_mmcr.h"
 
 struct mec_mbox_info {
     uint32_t base_addr;
@@ -46,15 +39,13 @@ static struct mec_mbox_info const *find_mbox_info(uint32_t base_addr)
 
 /* ---- Public API ---- */
 
-int mec_hal_mbox_init(struct mec_mbox_regs *base, uint32_t swi_ien_msk, uint32_t flags)
+int mec_hal_mbox_init(uintptr_t regbase, uint32_t swi_ien_msk, uint32_t flags)
 {
-    const struct mec_mbox_info *mbi = find_mbox_info((uint32_t)base);
+    const struct mec_mbox_info *mbi = find_mbox_info((uint32_t)regbase);
 
-    if (!mbi) {
+    if (mbi == NULL) {
         return MEC_RET_ERR_INVAL;
     }
-
-    struct mec_mbox_regs *const regs = (struct mec_mbox_regs *)mbi->base_addr;
 
     mec_hal_pcr_clr_blk_slp_en(mbi->pcr_id);
     mec_hal_girq_ctrl(mbi->devi, 0);
@@ -62,12 +53,12 @@ int mec_hal_mbox_init(struct mec_mbox_regs *base, uint32_t swi_ien_msk, uint32_t
     if (flags & MEC_MBOX_FLAG_RESET) {
         mec_hal_pcr_blk_reset(mbi->pcr_id);
     } else {
-        regs->H2EC = 0xffu; /* clear EC MBOX interrupt status */
+        mmcr8_wr(0xffu, regbase + MBOX_H2E_OFS); /* clear EC MBOX interrupt status */
     }
 
     mec_hal_girq_clr_src(mbi->devi);
 
-    regs->ECSMIM = (uint8_t)(swi_ien_msk & 0xffu);
+    mmcr8_wr((uint8_t)(swi_ien_msk & 0xffu), regbase + MBOX_SMI_IM_OFS);
 
     if (flags & MEC_MBOX_FLAG_INTR_EN) {
         mec_hal_girq_ctrl(mbi->devi, 1);
@@ -76,11 +67,11 @@ int mec_hal_mbox_init(struct mec_mbox_regs *base, uint32_t swi_ien_msk, uint32_t
     return MEC_RET_OK;
 }
 
-int mec_hal_mbox_girq_ctrl(struct mec_mbox_regs *base, uint8_t enable)
+int mec_hal_mbox_girq_ctrl(uintptr_t regbase, uint8_t enable)
 {
-    const struct mec_mbox_info *mbi = find_mbox_info((uint32_t)base);
+    const struct mec_mbox_info *mbi = find_mbox_info((uint32_t)regbase);
 
-    if (!mbi) {
+    if (mbi == NULL) {
         return MEC_RET_ERR_INVAL;
     }
 
@@ -89,11 +80,11 @@ int mec_hal_mbox_girq_ctrl(struct mec_mbox_regs *base, uint8_t enable)
     return MEC_RET_OK;
 }
 
-int mec_hal_mbox_girq_clr(struct mec_mbox_regs *base)
+int mec_hal_mbox_girq_clr(uintptr_t regbase)
 {
-    const struct mec_mbox_info *mbi = find_mbox_info((uint32_t)base);
+    const struct mec_mbox_info *mbi = find_mbox_info((uint32_t)regbase);
 
-    if (!mbi) {
+    if (mbi == NULL) {
         return MEC_RET_ERR_INVAL;
     }
 
@@ -102,140 +93,142 @@ int mec_hal_mbox_girq_clr(struct mec_mbox_regs *base)
     return MEC_RET_OK;
 }
 
-uint32_t mec_hal_mbox_girq_result(struct mec_mbox_regs *base)
+uint32_t mec_hal_mbox_girq_result(uintptr_t regbase)
 {
-    const struct mec_mbox_info *mbi = find_mbox_info((uint32_t)base);
+    const struct mec_mbox_info *mbi = find_mbox_info((uint32_t)regbase);
 
-    if (!mbi) {
+    if (mbi == NULL) {
         return 0;
     }
 
     return mec_hal_girq_result(mbi->devi);
 }
 
-int mec_hal_mbox_sirq_set(struct mec_mbox_regs *base, uint8_t bitmap)
+int mec_hal_mbox_sirq_set(uintptr_t regbase, uint8_t bitmap)
 {
-    if (!base) {
+    if (regbase != MEC_MBOX0_BASE) {
         return MEC_RET_ERR_INVAL;
     }
 
-    if (bitmap) {
-        base->ECSMIT |= bitmap;
+    if (bitmap != 0) {
+        mmcr8_wr(bitmap, regbase + MBOX_SMI_IS_OFS);
     }
 
     return MEC_RET_OK;
 }
 
-int mec_hal_mbox_sirq_en_mask(struct mec_mbox_regs *base, uint8_t val, uint8_t mask)
+int mec_hal_mbox_sirq_en_mask(uintptr_t regbase, uint8_t val, uint8_t mask)
 {
-    if (!base) {
+    if (regbase != MEC_MBOX0_BASE) {
         return MEC_RET_ERR_INVAL;
     }
 
-    if (mask) {
-        base->ECSMIM = (base->ECSMIM & ~mask) | (val & mask);
+    if (mask != 0) {
+        mmcr8_update_field(regbase + MBOX_SMI_IM_OFS, val, mask);
     }
 
     return MEC_RET_OK;
 }
 
-int mec_hal_mbox_get_host_to_ec(struct mec_mbox_regs *base, uint8_t *data)
+int mec_hal_mbox_get_host_to_ec(uintptr_t regbase, uint8_t *data)
 {
-    if (!base) {
+    if (regbase != MEC_MBOX0_BASE) {
         return MEC_RET_ERR_INVAL;
     }
 
-    uint8_t host_to_ec = base->H2EC;
+    uint8_t host_to_ec = mmcr8_rd(regbase + MBOX_H2E_OFS);
 
-    if (data) {
+    if (data != NULL) {
         *data = host_to_ec;
     }
 
     return MEC_RET_OK;
 }
 
-int mec_hal_mbox_set_host_to_ec(struct mec_mbox_regs *base, uint8_t data)
+int mec_hal_mbox_set_host_to_ec(uintptr_t regbase, uint8_t data)
 {
-    if (!base) {
+    if (regbase != MEC_MBOX0_BASE) {
         return MEC_RET_ERR_INVAL;
     }
 
-    base->H2EC = data;
+    mmcr8_wr(data, regbase + MBOX_H2E_OFS);
 
     return MEC_RET_OK;
 }
 
-int mec_hal_mbox_get_ec_to_host(struct mec_mbox_regs *base, uint8_t *data)
+int mec_hal_mbox_get_ec_to_host(uintptr_t regbase, uint8_t *data)
 {
-    if (!base) {
+    if (regbase != MEC_MBOX0_BASE) {
         return MEC_RET_ERR_INVAL;
     }
 
-    uint8_t ec_to_host = base->EC2H;
+    uint8_t ec_to_host = mmcr8_rd(regbase + MBOX_E2H_OFS);
 
-    if (data) {
+    if (data != NULL) {
         *data = ec_to_host;
     }
 
     return MEC_RET_OK;
 }
 
-int mec_hal_mbox_set_ec_to_host(struct mec_mbox_regs *base, uint8_t data)
+int mec_hal_mbox_set_ec_to_host(uintptr_t regbase, uint8_t data)
 {
-    if (!base) {
+    if (regbase != MEC_MBOX0_BASE) {
         return MEC_RET_ERR_INVAL;
     }
 
-    base->EC2H = data;
+    mmcr8_wr(data, regbase + MBOX_E2H_OFS);
 
     return MEC_RET_OK;
 }
 
-int mec_hal_mbox_get(struct mec_mbox_regs *base, uint8_t mbox, uint8_t *data)
+int mec_hal_mbox_get(uintptr_t regbase, uint8_t mbox, uint8_t *data)
 {
-    if (!base || (mbox > MEC_MBOX_MAX_INDEX) || !data) {
+    if ((regbase != MEC_MBOX0_BASE) || (data == NULL) || (mbox > MEC_MBOX_MAX_INDEX)) {
         return MEC_RET_ERR_INVAL;
     }
 
-    volatile uint8_t *p = (volatile uint8_t *)&base->MBOXD[0];
-
-    *data = p[mbox];
+    *data = mmcr8_rd(regbase + MBOX_MB_OFS(mbox));
 
     return MEC_RET_OK;
 }
 
-int mec_hal_mbox_put(struct mec_mbox_regs *base, uint8_t mbox, uint8_t data)
+int mec_hal_mbox_put(uintptr_t regbase, uint8_t mbox, uint8_t data)
 {
-    if (!base || (mbox > MEC_MBOX_MAX_INDEX)) {
+    if ((regbase != MEC_MBOX0_BASE) || (mbox > MEC_MBOX_MAX_INDEX)) {
         return MEC_RET_ERR_INVAL;
     }
 
-    volatile uint8_t *p = (volatile uint8_t *)&base->MBOXD[0];
-
-    p[mbox] = data;
+    mmcr8_wr(data, regbase + MBOX_MB_OFS(mbox));
 
     return MEC_RET_OK;
 }
 
 /* 32 8-bit mailboxes are grouped as 8 32-bit registers */
-int mec_hal_mbox32_get(struct mec_mbox_regs *base, uint8_t mbox, uint32_t *data)
+int mec_hal_mbox32_get(uintptr_t regbase, uint8_t mbox, uint32_t *data)
 {
-    if (!base || (mbox > (MEC_MBOX_MAX_INDEX / 4)) || !data) {
+    uint32_t mbox_ofs = MBOX_MB_OFS(0);
+
+    if ((regbase != MEC_MBOX0_BASE) || (data == NULL) || (mbox > (MEC_MBOX_MAX_INDEX / 4))) {
         return MEC_RET_ERR_INVAL;
     }
 
-    *data = base->MBOXD[mbox];
+    mbox_ofs += (uint32_t)mbox * 4u;
+    *data = mmcr32_rd(regbase + mbox_ofs);
 
     return MEC_RET_OK;
 }
 
-int mec_hal_mbox32_put(struct mec_mbox_regs *base, uint8_t mbox, uint32_t data)
+int mec_hal_mbox32_put(uintptr_t regbase, uint8_t mbox, uint32_t data)
 {
-    if (!base || (mbox > (MEC_MBOX_MAX_INDEX / 4))) {
+    uint32_t mbox_ofs = MBOX_MB_OFS(0);
+
+    if ((regbase != MEC_MBOX0_BASE) || (mbox > (MEC_MBOX_MAX_INDEX / 4))) {
         return MEC_RET_ERR_INVAL;
     }
 
-    base->MBOXD[mbox] = data;
+    mbox_ofs += (uint32_t)mbox * 4u;
+    mmcr32_wr(data, regbase + mbox_ofs);
 
     return MEC_RET_OK;
 }
